d34efca67e7cb017ea9e219a0775b3a921452a11
ALEXANDER MOSS <alexandermoss@KuthedkMBP.local>
2014-08-11 19:14:35 -0500

    changes

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-FooCoin (FOO)
+NikephorosCoin (NPC)
 ===========
-
-[![Build Status](https://travis-ci.org/RazorLove/foocoin.png?branch=master)](https://travis-ci.org/RazorLove/foocoin)
 
 
 Scrypt Hashcash PoW Template
--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #foocoin2TEST3\r");
-            Send(hSocket, "WHO #foocoin2TEST3\r");
+            Send(hSocket, "JOIN #nikephoroscoin2TEST3\r");
+            Send(hSocket, "WHO #nikephoroscoin2TEST3\r");
         } else {
-            // randomly join #foocoin00-#foocoin99
+            // randomly join #nikephoroscoin00-#nikephoroscoin99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #foocoin2%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #foocoin2%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #nikephoroscoin2%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #nikephoroscoin2%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
